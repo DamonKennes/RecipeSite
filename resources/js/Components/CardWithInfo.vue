35a8e7eac1e17eb7f9c16b414015f92987708372
DamonKennes <damon.kennes@student.kuleuven.be>
2022-12-02 16:19:56 +0100

    Eerste versie Card components

---
+++ resources/js/Components/CardWithInfo.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="flex">
+      <Card
+      :image = imageRecipe
+      :name = name
+      :score = score
+      />
+      <GrafiekCard 
+      :image = imageGraf
+       />
+    </div>
+</template>
+  
+  
+  <script>
+  import Card from './Card.vue';
+  import GrafiekCard from './GrafiekCard.vue';
+
+  export default {
+    name: 'CardWithInfo',
+    components: {
+        Card,
+        GrafiekCard,
+    },
+    props: {
+        score: Number,
+        name: String,
+        imageGraf: String,
+        imageRecipe: String,
+    },
+    methods: {
+      onClick() {
+        console.log('click');
+        //this.$emit('open-recipe');
+      },
+    },
+  };
+  </script>
---
+++ resources/js/Components/GrafiekCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div  class="flex p-10">  
+      <div v-on:click="onClick()" class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" v-bind:src= image alt="Pasta">
+      </div>
+    </div>
+  </template>
+    
+
+    <script>
+    export default {
+      name: 'GrafiekCard',
+      components: {
+      },
+      props: {
+        image: String,
+      },
+      methods: {
+        onClick() {
+          console.log('click grafiek');
+          //this.$emit('open-recipe');
+        },
+      },
+    };
+    </script>
