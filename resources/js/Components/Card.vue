df64b243473b7a4472ea35e376818d547198b139
Pieter Claeys <pieter.claeys@me.com>
2022-12-28 09:56:07 +0100

    layout fix

--- resources/js/Components/Card.vue
+++ resources/js/Components/Card.vue
@@ -3,7 +3,7 @@
     <div v-on:click="onClick()" class="w-40 rounded overflow-hidden shadow-lg h-full cursor-pointer flex flex-col justify-between">
         <div>
       <img class="w-full" v-bind:src = image alt="Image">
-        <div class="font-bold text-xl px-6 pt-4">{{name}}</div>
+        <div class="font-bold text-md px-6 pt-4">{{name}}</div>
         </div>
         <Stars class="mt-auto px-6 pb-4" :initial-rating="score" @change-rating="updateRating"></Stars>
     </div>
